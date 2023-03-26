9b10d9ea2f04b9a7743291552dfad747502f54b3
Angel <43154124+angel-utn@users.noreply.github.com>
2023-03-26 12:42:25 -0300

    Archivos de la semana 01

---
+++ Semana 01/EJERCICIO.psc
@@ -0,0 +1,8 @@
+Algoritmo sin_titulo
+	Escribir "INGRESE EL PRIMER NUMERO: "
+	Leer NUM1
+	Escribir "INGRESE EL SEGUNDO NUMERO: "
+	Leer NUM2
+	SUMA = NUM1 + NUM2
+	Escribir "LA SUMA ES: ", SUMA
+FinAlgoritmo
---
+++ Semana 01/ej01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n1, n2, resultado;
+  cin >> n1;
+  cin >> n2;
+  resultado = n1 + n2;
+  cout << "Resultado: ";
+  cout << resultado;
+  return 0;
+}
