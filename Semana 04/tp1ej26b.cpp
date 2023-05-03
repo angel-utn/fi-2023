68b58b9a1e9130e4c29bccaebeeeffbfbcfec2bd
Angel <43154124+angel-utn@users.noreply.github.com>
2023-05-03 19:20:05 -0300

    Add files via upload

---
+++ Semana 04/tp1ej26b.cpp
@@ -0,0 +1,21 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    int num;
+    cout<<"INGRESE UN "NUMERO:" ";
+    cin>>num;
+    num=num*2;
+    cout<<"EL DOBLE ES "<<num;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
