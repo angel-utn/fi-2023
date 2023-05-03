ca91534a2648a97d335faa5f6f9b5229f4ff3042
Angel <43154124+angel-utn@users.noreply.github.com>
2023-05-03 19:18:17 -0300

    Add files via upload

---
+++ Semana 07/ej22tp3.cpp
@@ -0,0 +1,31 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    int i, codigo, codigoMaximo;
+    float precio, precioMinimo, precioMaximo=0;
+    for(i=1;i<=5;i++){
+        cout<<"CODIGO ";
+        cin>>codigo;
+        cout<<"PRECIO ";
+        cin>>precio;
+        if(precio>precioMaximo){
+            precioMaximo=precio;
+            codigoMaximo=codigo;
+        }
+
+    }
+    cout<<"CODIGO DEL ARTICULO MAS CARO "<<codigoMaximo;
+	cout<<endl;
+	//cout<<"PRECIO DEL ARTICULO MAS BARATO "<<precioMinimo;
+	system("pause");
+	return 0;
+}
